bd160755f6aafc12c3ec145cd20a72141ded2978
dzaky1201 <dzakyhdr@gmail.com>
2021-12-13 10:03:29 +0700

    fileupload

---
+++ app/Http/Controllers/FileUploadController.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class FileUploadController extends Controller
+{
+    public function fileUpload()
+    {
+        return view('file-upload');
+    }
+    public function prosesFileUpload(Request $request)
+    {
+        $request->validate([
+            'berkas' => 'required|file|mimes:pdf,png,jpg,jpeg|max:1000',
+        ]);
+        $extFile = $request->berkas->getClientOriginalExtension();
+        $namaFile = 'halal-' . time() . "." . $extFile;
+        $request->berkas->storeAs('public', $namaFile);
+        $pathBaru = asset('storage/' . $namaFile);
+        echo "Proses upload berhasil, file berada di: <a href='$pathBaru'>
+        $pathBaru</a>";
+    }
+
+    public function fileUploadRename()
+    {
+        return view('file-upload-rename');
+    }
+
+    public function prosesFileUploadRename(Request $request)
+    {
+        $request->validate([
+            'nama_gambar' => 'required|min:5|alpha_dash',
+            'gambar_profile' => 'required|file|image|max:1000',
+        ]);
+        // ambil nama extension file asal
+        $extFile = $request->gambar_profile->getClientOriginalExtension();
+        // generate nama file akhir, diambil dari inputan nama_gambar + extension
+        $namaFile = $request->nama_gambar . "." . $extFile;
+        // pindahkan file upload ke folder storage/app/public/gambar/
+        $request->gambar_profile->storeAs('public/gambar', $namaFile);
+        // generate path gambar yang bisa diakses (path di folder public)
+        $pathPublic = asset('storage/gambar/' . $namaFile);
+        echo "Gambar berhasil di upload ke <a href=" . $pathPublic . ">$namaFile</a>";
+        echo "<br>";
+        echo "<img src=" . $pathPublic . " width='200px'>";
+    }
+}
---
+++ routes/web.php
@@ -0,0 +1,26 @@
+<?php
+
+use App\Http\Controllers\FileUploadController;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/file-upload', [FileUploadController::class, 'fileUpload']);
+Route::post('/file-upload', [FileUploadController::class, 'prosesfileUpload']);
+Route::get('/file-upload-rename', [
+    FileUploadController::class,
+    'fileUploadRename'
+]);
+Route::post('/file-upload-rename', [
+    FileUploadController::class,
+    'prosesFileUploadRename'
+]);
